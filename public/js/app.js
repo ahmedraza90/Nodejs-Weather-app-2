22d3eb527e42322bb92fe10d4932bb8d6b342dfc
ahmed <34ahmed.raza@gmail.com>
2021-10-01 13:39:35 -0700

    app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-const slocation = document.querySelector('form')
-const search    = document.querySelector('input')
-const one_p     = document.querySelector('#one-p')
-const two_p     = document.querySelector('#two-p')
+const location = document.querySelector('form')
+const search   = document.querySelector('input')
+const one_p    = document.querySelector('#one-p')
+const two_p    = document.querySelector('#two-p')
 
 
-slocation.addEventListener('submit',(e)=>{
+location.addEventListener('submit',(e)=>{
     e.preventDefault()
     
     const loc =  search.value

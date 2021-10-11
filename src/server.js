56f81d114d8893fc9687b698864e28e0355a6625
ahmedraza90 <61561367+ahmedraza90@users.noreply.github.com>
2021-10-11 14:39:23 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -51,6 +51,10 @@ app.get('/weather',(req,res)=>{
 // In Node.js, process.env is a global variable that is injected during runtime.
 // It is a view of the state of the system environment variables. When we set an environment variable, 
 // it is loaded into process.env during runtime and can later be accessed.
+// When hosting your application on another service (like Heroku, Nodejitsu, and AWS), your host may independently configure the process.env.PORT variable for you
+// you can set the environment variable PORT to tell your web server what port to listen on.
+// process.env.PORT || 3000 means: whatever is in the environment variable PORT, or 3000 if there's nothing there.
+// If you pass 3000 hard-coded to app.listen(), you're always listening on port 3000, which might be just for you, or not, depending on your requirements and the requirements of the environment in which you're running your server.
 const port = process.env.PORT || 3000
 app.listen(port,(port)=>{
     console.log(`server is up on ${port}`)
